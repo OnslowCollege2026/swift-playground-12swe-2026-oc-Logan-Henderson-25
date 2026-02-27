058942310cb5861584879a1b3116785dedaacade
oc-Logan-Henderson-25 <logan.henderson@student.onslow.school.nz>
2026-02-27 01:53:52 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,38 +1,46 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+///This function lets the user interact with the menu
+/// - The function returns what the user chose
+func menuChoice() -> Int {
+    while true {
+        print("""
+        ==== Egg Shop ====
+        1. Add eggs
+        2. Sell eggs
+        3. Show current stock
+        4. Show total eggs sold
+        5. Exit
+        Choose an option:
+        """)
 
-func printRoomIntro() {
 
-    print("This program calculates room area and volume.")
-    print("It also finds the usable space after furniture.")
+    if let input = readLine(), let choice = Int(input) {
+        return choice
+    }else {
+        return 0
+    }
+
+    }
 }
 
-func printDivider() {
-    print("--------")
+func addEggs(currentStock: Int, amount: Int) -> Int{
+if amount < 1, amount + currentStock > 1000 {
+    print("Invalid amount, eggs must be between 1 and 1000")
+    return currentStock
+}
+    let newStock = amount + currentStock
+    print("You have added \(amount) eggs to your stock.")
+    return newStock
 }
 
-func printUnitsNote() {
-    print("All measurements are in metres.")
-}
 
-func printGoodbye() {
-    print("Done. Thanks for using the calculator.")
-}
 
 @main
 struct SwiftPlayground {
     static func main() {
 
-printRoomIntro()
-
-printDivider()
-print("Summary here")
-printDivider()
-
-printUnitsNote()
-
-printGoodbye()
 
     }
 }
