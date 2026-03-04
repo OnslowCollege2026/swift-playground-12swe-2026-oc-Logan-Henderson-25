5c9e09d45dcf5d9afd73c60babaef453b7148732
oc-Logan-Henderson-25 <logan.henderson@student.onslow.school.nz>
2026-03-04 22:13:16 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -15,7 +15,7 @@ func menuChoice() -> Int {
         Choose an option:
         """)
 
-
+/// This lets the user interact with the menu
     if let input = readLine(), let choice = Int(input) {
         return choice
     }else {
@@ -40,15 +40,68 @@ if amount < 1, amount + currentStock > 1000 {
     return newStock
 }
 
+/// This function lets the user sell eggs from there stock
+/// - Parameters
+///     - currentStock is how many eggs the user currently has in their stock
+///     - amount is the amount of eggs the user is selling
+/// - This function returns the amount of eggs the user has sold
 func sellEggs(currentStock: Int, amount: Int) -> Int?{
-    if amount < 1, amount > currentStock {}
+        if amount < 1, amount > currentStock { return 0}
+        let remainingStock = currentStock - amount
+        return remainingStock
+    }
 
+/// This function shows the user how many eggs they have sold in total
+/// - Parameters
+///     - currentSold is the amount of eggs that the user has sold
+///     - amount is the amount of egg that the user is selling
+/// - This funtion returns the new amount of eggs that the user has sold
+func updateSoldCount(currentSold: Int, amount: Int) -> Int {
+    let newSold = currentSold + amount
+    return newSold
+}
+
+/// This function tells the user how many eggs they have left in their stock
+/// - Parameter: stock is the amount of eggs that the user has
+/// - This function returns the stock message in a string
+func stockMessage(stock: Int) -> String {
+    let stockMessage = "You have \(stock) eggs left"
+    return stockMessage
 }
 
 @main
 struct SwiftPlayground {
     static func main() {
 
+    var eggsInStock:Int = 0
+    var eggsSold:Int = 0
+    var running:Bool = true
+
+    while running {
+        let userChoice = menuChoice()
+    
+        switch userChoice {
+            case 1:
+                print("How many eggs would you like to add?")
+            case 2:
+                print("How many eggs would you like to sell?")
+                if let userInput = readLine(), let eggsBeingSold = Int(userInput) {
+                    if let newStock = sellEggs(currentStock: eggsInStock, amount: eggsBeingSold) {
+                        eggsInStock = newStock
+                        eggsSold = updateSoldCount(currentSold: eggsSold, amount: eggsBeingSold)
+                    }
+                }
+            case 3:
+                print(stockMessage(stock: eggsInStock))
+            case 4:
+                print(" You have sold a total of \(eggsSold)")
+            case 5:
+                running = false
+                print("You have sold \(eggsSold) eggs, and have \(eggsInStock) left in your stock, thank you for using the Egg Shop.")
+            default:
+                print("Invalid option, please try again")
+        }
+    }
 
     }
 }
