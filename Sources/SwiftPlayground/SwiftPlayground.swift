8bbc10f99df3a4683293aaa74c9f0ca6bfd4bf8c
oc-Logan-Henderson-25 <logan.henderson@student.onslow.school.nz>
2026-03-06 01:59:24 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -31,10 +31,16 @@ func menuChoice() -> Int {
 ///     - amount is how many eggs the user is adding to their stock
 /// - This function returns the amount of eggs the user has added to their stock
 func addEggs(currentStock: Int, amount: Int) -> Int{
-if amount < 1, amount + currentStock > 1000 {
-    print("Invalid amount, eggs must be between 1 and 1000")
+if amount < 1 {
+    print("Invalid amount, eggs must be at least 1")
     return currentStock
 }
+
+if currentStock + amount > 1000 {
+    print("Invalid amount, eggs must be below 1000")
+    return currentStock
+}
+
     let newStock = amount + currentStock
     print("You have added \(amount) eggs to your stock.")
     return newStock
@@ -46,9 +52,19 @@ if amount < 1, amount + currentStock > 1000 {
 ///     - amount is the amount of eggs the user is selling
 /// - This function returns the amount of eggs the user has sold
 func sellEggs(currentStock: Int, amount: Int) -> Int?{
-        if amount < 1, amount > currentStock { return 0}
-        let remainingStock = currentStock - amount
-        return remainingStock
+    if amount < 1 {
+    print("Invalid amount, you can not sell negative eggs")
+    return currentStock
+}
+
+if amount > currentStock {
+    print("Invalid amount, you can not sell more eggs that you have")
+    return currentStock
+}
+
+    let newStock = currentStock - amount
+    print("You have sold \(amount) eggs from your stock.")
+    return newStock
     }
 
 /// This function shows the user how many eggs they have sold in total
@@ -73,16 +89,24 @@ func stockMessage(stock: Int) -> String {
 struct SwiftPlayground {
     static func main() {
 
+    // Tells the user how many eggs they have sold.
     var eggsInStock:Int = 0
     var eggsSold:Int = 0
+    // Makes sure the programme runs.
     var running:Bool = true
+
 
     while running {
         let userChoice = menuChoice()
     
         switch userChoice {
+            // Case 1 lets the user add eggs to their stock
             case 1:
                 print("How many eggs would you like to add?")
+                if let userInput = readLine(), let eggsBeingAdded = Int(userInput) {
+                    eggsInStock = addEggs(currentStock: eggsInStock, amount: eggsBeingAdded)
+                }
+            // Case 2 lets the user sell eggs from their stock
             case 2:
                 print("How many eggs would you like to sell?")
                 if let userInput = readLine(), let eggsBeingSold = Int(userInput) {
@@ -91,13 +115,17 @@ struct SwiftPlayground {
                         eggsSold = updateSoldCount(currentSold: eggsSold, amount: eggsBeingSold)
                     }
                 }
+            // Case 3 lets the user see how many eggs they have left in their stock
             case 3:
                 print(stockMessage(stock: eggsInStock))
+            // Case 4 lets the user see how many eggs they have sold
             case 4:
                 print(" You have sold a total of \(eggsSold)")
+            // Case 5 lets the user end the programme
             case 5:
                 running = false
                 print("You have sold \(eggsSold) eggs, and have \(eggsInStock) left in your stock, thank you for using the Egg Shop.")
+            // default shows the user a error message if they enter anything that isn't 1-5
             default:
                 print("Invalid option, please try again")
         }
