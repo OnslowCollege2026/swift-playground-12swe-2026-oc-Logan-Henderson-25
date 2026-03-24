97538aaf61dfed08943e66fd234a5553143a5d4f
oc-Logan-Henderson-25 <logan.henderson@student.onslow.school.nz>
2026-03-24 00:16:13 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,43 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let vocabulary = [
+            ["Hello",       "Hola", "Me llamo", "Gracias", "Adios", "Si"],
+            ["My name is",  "Me llamo", "Hola", "Gracias", "Adios", "Si"],
+            ["Thank you",   "Gracias", "Hola", "Me llamo", "Adios", "Si"],
+            ["Goodbye",     "Adios", "Hola", "Me llamo", "Gracias", "Si"],
+            ["Yes",         "Si", "Hola", "Me llamo", "Gracias", "Adios"]
+        ]
+
+        var incorrectIndices: [Int] = []
+        var incorrectCount = 0
+        var correctCount = 0
+
+        while correctCount < vocabulary.count {
+            let correctAnswer = vocabulary[correctCount][1]
+            var options = Array(vocabulary[correctCount][1...5]).shuffled()
+            
+            print("What is the Spanish word for \(vocabulary[correctCount][0])?")
+            for (index, option) in options.enumerated() {
+                print("\(index + 1). \(option)")
+            }
+            print("Enter your answer: ")
+            
+            if let userInput = readLine() {
+                if let userAnswer = Int(userInput), userAnswer > 0, userAnswer <= 5 {
+                    if options[userAnswer - 1] == correctAnswer {
+                        print("That is correct!")
+                        correctCount += 1
+                    } else {
+                        print("That is incorrect. The correct answer is \(correctAnswer).")
+                        incorrectIndices.append(correctCount)
+                        incorrectCount += 1
+                        correctCount += 1
+                    }
+                } else {
+                    print("Invalid input. Please enter your answers number.")
+                }
+            }
+        }
     }
 }
