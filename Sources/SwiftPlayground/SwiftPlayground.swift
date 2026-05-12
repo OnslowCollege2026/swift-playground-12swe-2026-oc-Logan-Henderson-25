32a45031ea52b92e8e3ee6526250caf8e13c89ae
oc-Logan-Henderson-25 <logan.henderson@student.onslow.school.nz>
2026-05-12 01:14:43 +0000

    Commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -13,16 +13,19 @@ let kumaraPricePerKg = 3.0 /// The price of kumara per kilogram.
 let kumaraweight = 0.1 /// The weight of a single kumara.
 let kumaraAdded = Double(readLine()!) /// Converts the user input into a double
 
+// This function lets the user select how much kumara they wish to purchase and stops the user from adding more/less kumara than is available in stock and prints appropriate messages based on the input.
 func kumaraBoundaries(kumaraAdded: Double) {
     if kumaraAdded < 0 {print("You cannot add a negative amount of kumara to your bag. Please try again.")}
     if kumaraAdded > 0 && kumaraAdded <= 50 {print("You have added \(kumaraAdded) kumara to your bag. Please enter again to calculate the weight and price of the kumara you have added to your bag.")}
     if kumaraAdded > 50 {print("Sorry, we only have \(kumaraStock) kilograms of kumara in stock. Please try again.")}
 
+// This function calculates the weight of users kumara and prints it out to the user.
 func kumaraWeight(kumaraAdded: Double) {
     let kumaraWeight = kumaraAdded * kumaraweight
     print("The weight of the kumara you have added to your bag is \(kumaraWeight * kumaraAdded) kilograms.")
 }
 
+// This function calculates the price of the users kumara and prints it out to the user.
 func kumaraPrice(kumaraAdded: Double) {
     let kumaraPrice = kumaraAdded * kumaraPricePerKg
     print("The price of the kumara you have added to your bag is $\(kumaraPrice).")
@@ -31,20 +34,21 @@ func kumaraPrice(kumaraAdded: Double) {
 struct SwiftPlayground {
     static func main() {
 
-// Prints a message that lets the user enter the weight of the kumara they wish to purchase in kilograms.
 
+// Prints the welcome message, the amount of kumara in stock and the price per kilogram, then tells the user to enter the weight of kumara they wish to purchase.
     print("Welcome to the roadside kumara stall!")
     print("We have \(kumaraStock) kilograms of kumara in stock, and the price is $\(kumaraPricePerKg) per kilogram.")
     print("Please enter the weight of the kumara you wish to purchase:")
 
+// This calls on the functions
 let kumaraAdded = Double(readLine()!)!
 kumaraBoundaries(kumaraAdded: kumaraAdded)
 kumaraWeight(kumaraAdded: kumaraAdded)
 kumaraPrice(kumaraAdded: kumaraAdded)
 
+// This prints the thank you message at the end of the program.
 print("Thank you for your purchase! Please come again.")
-
 
         }
     }
-    }
+}
